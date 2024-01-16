259ea5eb1f384a7335db000f0489165b26f6a1d4
Haram0111 <5gkfka5@naver.com>
2024-01-16 22:48:19 +0900

    section4 Local Test Success

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -38,10 +38,22 @@ class MemberServiceTest {
     @Test
     public void 중복_회원_예외() throws Exception{
         //given
+        Member member1 = new Member();
+        member1.setName("Kem");
+
+        Member member2 = new Member();
+        member2.setName("Kem");
 
         //when
+        memberService.join(member1);
+        try{
+            memberService.join(member2);
+        }catch (IllegalStateException e){
+            return;
+        }
 
         //then
+        fail("예외가 발생해야 한다");
     }
 
 }
